20f2512a18a13ec0c9fe78f5b5865247af50d200
Jon C <me@jonc.dev>
2024-07-07 15:58:54 +0100

    Allow generating a keypair during program build

--- build.zig
+++ build.zig
@@ -27,22 +27,13 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
     b.installArtifact(lib);
 
-    const exe = buildKeygenExe(b, target, optimize);
+    const exe = buildKeygenExe(b, "base58-keygen", target, optimize);
+    // Add a host version of keygen to be used during program build
+    _ = buildKeygenExe(b, "base58-keygen-host", b.graph.host, optimize);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
     const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
 
     // This allows the user to pass arguments to the application in the build
@@ -56,14 +47,11 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
     const lib_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
-
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const exe_unit_tests = b.addTest(.{
@@ -74,17 +62,14 @@ pub fn build(b: *std.Build) void {
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
 }
 
-fn buildKeygenExe(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) *std.Build.Step.Compile {
+fn buildKeygenExe(b: *std.Build, name: []const u8, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) *std.Build.Step.Compile {
     const exe = b.addExecutable(.{
-        .name = "base58",
+        .name = name,
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
@@ -96,20 +81,18 @@ fn buildKeygenExe(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std
     });
     const clap_mod = clap_dep.module("clap");
     exe.root_module.addImport("clap", clap_mod);
-
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
     b.installArtifact(exe);
 
     return exe;
 }
 
+// Helper function for generating a keypair during a program build, similar to
+// the base Solana tools.
+// Looks for an artifact called "base58-keygen-host", which is just like
+// "base58-keygen", except it targets the host instead of a BPF / SBF target.
 pub fn generateKeypairRunStep(b: *std.Build, path: []const u8) *std.Build.Step.Run {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
-    const exe = buildKeygenExe(b, target, optimize);
+    const base58_dep = b.dependency("base58", .{});
+    const exe = base58_dep.artifact("base58-keygen-host");
     const run_exe = b.addRunArtifact(exe);
     run_exe.addArg("-o");
     run_exe.addFileArg(.{ .path = path });
