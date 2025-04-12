c0440b2888d0ab3cc4dec52a459c1279a8c442cc
Jon C <me@jonc.dev>
2025-04-12 19:35:44 +0200

    Also update CI and test-build

--- test-build/build.zig
+++ test-build/build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const program = b.addSharedLibrary(.{
-        .name = "test-build",
+        .name = "test_build",
         .root_source_file = b.path("src/main.zig"),
         .optimize = optimize,
         .target = target,
