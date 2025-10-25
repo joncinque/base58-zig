9ee061f341c73d2656296b15c2c773549fee059a
Jon C <me@jonc.dev>
2025-10-25 23:19:48 +0200

    Update to Zig v0.15.2
    #### Problem
    
    This package is still on Zig v0.14, but Zig v0.15 has been out for some
    time.
    
    #### Summary of changes
    
    Upgrade everything required to work with Zig 0.15.

--- build.zig
+++ build.zig
@@ -16,18 +16,10 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     // Export self as a module
-    _ = b.addModule("base58", .{ .root_source_file = b.path("src/root.zig") });
-
-    const lib = b.addStaticLibrary(.{
-        .name = "base58",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
+    const mod = b.addModule("base58", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
-        .optimize = optimize,
     });
-
-    b.installArtifact(lib);
 
     const exe = buildKeygenExe(b, "base58-keygen", target, optimize);
     // Add a host version of keygen to be used during program build
@@ -48,31 +40,22 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = mod,
     });
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
 }
 
 fn buildKeygenExe(b: *std.Build, name: []const u8, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) *std.Build.Step.Compile {
     const exe = b.addExecutable(.{
         .name = name,
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
 
     const clap_dep = b.dependency("clap", .{
