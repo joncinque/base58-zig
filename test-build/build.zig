c037962d8f0172b7eeba41d2da80db89e668a92b
Jon C <me@jonc.dev>
2025-10-25 23:45:32 +0200

    Update to Zig v0.15.2 (#2)
    * Update to Zig v0.15.2
    
    #### Problem
    
    This package is still on Zig v0.14, but Zig v0.15 has been out for some
    time.
    
    #### Summary of changes
    
    Upgrade everything required to work with Zig 0.15.
    
    * Run CI less
    
    * Update test-build
    
    * Finish bumping everything, rename CI

--- test-build/build.zig
+++ test-build/build.zig
@@ -4,11 +4,14 @@ const base58 = @import("base58");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const program = b.addSharedLibrary(.{
+    const program = b.addLibrary(.{
         .name = "test_build",
-        .root_source_file = b.path("src/main.zig"),
-        .optimize = optimize,
-        .target = target,
+        .linkage = .dynamic,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
     base58.generateProgramKeypair(b, program);
     b.installArtifact(program);
